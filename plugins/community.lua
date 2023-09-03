be412fe1f1350b3c9b0048bd04698750583bcbfe
Zain Ahmad <zain@signzy.com>
2023-09-04 01:12:22 +0530

    first proper setup done

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,21 +1,34 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  -- "xiyaowong/transparent.nvim"
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.git.diffview-nvim"},
+  { import = "astrocommunity.pack.python"},
+  { import = "astrocommunity.pack.java"},
+  { import = "astrocommunity.pack.yaml"},
+  { import = "astrocommunity.pack.docker"},
+  { import = "astrocommunity.pack.markdown"},
+  { import = "astrocommunity.pack.toml"},
+  { import = "astrocommunity.pack.json"},
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.git.diffview-nvim"},
   { import = "astrocommunity.diagnostics.trouble-nvim"},
   { import = "astrocommunity.motion.hop-nvim"},
   { import = "astrocommunity.motion.nvim-spider"},
-  -- { import = "astrocommunity.split-and-window.minimap.nvim"},
-  { import = "astrocommunity.split-and-window.edgy-nvim"},
+  -- { import = "astrocommunity.split-and-window.minimap.vim"},
+  -- { import = "astrocommunity.split-and-window.edgy-nvim"},
   -- { import = "astrocommunity.workflow.bad-practices.nvim"},
   { import = "astrocommunity.programming-language-support.csv-vim"},
   -- { import = "astrocommunity.workflow.hard-time.nvim"},
   { import = "astrocommunity.colorscheme.everforest"},
-  { import = "astrocommunity.colorscheme.nightfox-nvim"},
+  -- { import = "astrocommunity.colorscheme.onedarkpro"},
+  -- { import = "astrocommunity.colorscheme.oxocarbon"},
+  { import = "astrocommunity.colorscheme.mini-base16"},
+  -- { import = "astrocommunity.colorscheme.nord"},
+  { import = "astrocommunity.colorscheme.melange-nvim"},
+  -- { import = "astrocommunity.color.cheme.iceberg"},
   { import = "astrocommunity.programming-language-support.nvim-jqx"}
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,94 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "xiyaowong/transparent.nvim",
+    event = "BufEnter",
+    config = function ()
+      require("transparent").setup({ -- Optional, you don't have to run setup.
+    groups = { -- table: default groups
+      'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+      'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+      'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+      'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+    },
+    extra_groups = {}, -- table: additional groups that should be cleared
+    exclude_groups = {}, -- table: groups you don't want to clear
+    })
+    end
+
+  },
+    {
+    "folke/styler.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("styler").setup {
+        themes = {
+          markdown = { colorscheme = "gruvbox" },
+          help = { colorscheme = "gruvbox" },
+        },
+      }
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    config = function(_, opts)
+      local tokyonight = require "tokyonight"
+      tokyonight.setup(opts)
+      tokyonight.load()
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    config = function()
+      require("gruvbox").setup()
+    end,
+  },
+  {
+  "wfxr/minimap.vim",
+  event = "BufEnter",
+  cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+  keys = {
+    { "<leader>um", "<cmd>MinimapToggle<CR>", desc = "Toggle minimap", mode = { "n" } },
+  },
+  init = function()
+    vim.g.minimap_width = 10
+    vim.g.minimap_auto_start = 1
+    vim.g.minimap_auto_start_win_enter = 1
+    vim.g.minimap_block_filetypes = {
+      "fugitive",
+      "nerdtree",
+      "tagbar",
+      "fzf",
+      "qf",
+      "netrw",
+      "NvimTree",
+      "lazy",
+      "mason",
+      "prompt",
+      "TelescopePrompt",
+      "noice",
+      "notify",
+      "neo-tree",
+    }
+    vim.g.minimap_highlight_search = 1
+    vim.g.minimap_git_colors = 1
+  end,
+}
 }
